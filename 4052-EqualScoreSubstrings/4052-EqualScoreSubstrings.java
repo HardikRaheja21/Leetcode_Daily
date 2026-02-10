4c2921ff5058c1d95c468fc6164abcb7e0ddcdf0
HardikRaheja21 <151737746+HardikRaheja21@users.noreply.github.com>
2026-02-10 15:09:57 +0530

    Create 4052-EqualScoreSubstrings

---
+++ 4052-EqualScoreSubstrings/4052-EqualScoreSubstrings.java
@@ -0,0 +1,16 @@
+// Last updated: 2/10/2026, 3:10:00 PM
+class Solution {
+    public boolean scoreBalance(String s) {
+        int totalSum = 0;
+        for(char ch : s.toCharArray()){
+            totalSum += (ch - 'a' + 1);
+        }
+        int leftSum = 0;
+        for(int i = 0; i < s.length(); i++){
+            leftSum += (s.charAt(i) - 'a' + 1);
+            int rightSum = totalSum - leftSum;
+            if(leftSum == rightSum) return true;
+        }
+        return false;
+    }
+}
