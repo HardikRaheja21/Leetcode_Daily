e9ccca27a372095ba5fb7f645c8538bfdba275f6
HardikRaheja21 <151737746+HardikRaheja21@users.noreply.github.com>
2026-02-10 15:19:20 +0530

    Create 90-SubsetsIi

---
+++ 90-SubsetsIi/90-SubsetsIi.java
@@ -0,0 +1,18 @@
+// Last updated: 2/10/2026, 3:19:23 PM
+class Solution {
+    private void subsets(int idx, int[] nums, List<List<Integer>> ans, List<Integer> ds){
+        ans.add(new ArrayList<>(ds));
+        for(int i = idx; i < nums.length; i++){
+            if(i != idx && nums[i] == nums[i- 1]) continue;
+            ds.add(nums[i]);
+            subsets(i + 1, nums, ans, ds);
+            ds.remove(ds.size() - 1);
+        }
+    }
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        subsets(0, nums, ans, new ArrayList<>());
+        return ans;
+    }
+}
