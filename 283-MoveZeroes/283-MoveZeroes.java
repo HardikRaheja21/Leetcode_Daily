dbfc584aaa20f79e0797418756b330c5966e93af
HardikRaheja21 <151737746+HardikRaheja21@users.noreply.github.com>
2026-02-10 15:17:41 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -0,0 +1,17 @@
+// Last updated: 2/10/2026, 3:17:44 PM
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i]  != 0){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        while(j < nums.length){
+            nums[j] = 0;
+            j++;
+        }
+    
+    }
+}
