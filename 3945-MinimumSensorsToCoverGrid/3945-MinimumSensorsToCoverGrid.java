ed93687f36e9c7cdc5d6c22ef310e7f66be89697
HardikRaheja21 <151737746+HardikRaheja21@users.noreply.github.com>
2026-02-10 15:10:26 +0530

    Create 3945-MinimumSensorsToCoverGrid

---
+++ 3945-MinimumSensorsToCoverGrid/3945-MinimumSensorsToCoverGrid.java
@@ -0,0 +1,9 @@
+// Last updated: 2/10/2026, 3:10:29 PM
+class Solution {
+    public int minSensors(int n, int m, int k) {
+        int cover = 2 * k + 1;
+        int rows = (n + cover - 1)/ cover;
+        int cols = (m + cover - 1) / cover;
+        return rows * cols;
+    }
+}
