db51bd0157cd902d75b2e54c1f57d089dac3862e
HardikRaheja21 <151737746+HardikRaheja21@users.noreply.github.com>
2026-02-10 15:20:08 +0530

    Create 42-TrappingRainWater

---
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -0,0 +1,21 @@
+// Last updated: 2/10/2026, 3:20:11 PM
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length;
+        int leftMax = 0, rightMax = 0, total = 0, left = 0, right = n - 1;
+        while(left < right){
+            if(height[left] <= height[right]){
+                if(leftMax > height[left]){
+                    total += leftMax - height[left];
+                } else leftMax = height[left];
+                left = left + 1;
+            } else {
+                if(rightMax > height[right]){
+                    total += rightMax - height[right];
+                } else rightMax = height[right];
+                right = right - 1;
+            }
+        }
+        return total;
+    }
+}
