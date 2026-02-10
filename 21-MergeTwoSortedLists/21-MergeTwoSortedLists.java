b9b1acb567f2cca05b28ccce9743ea9371a23132
HardikRaheja21 <151737746+HardikRaheja21@users.noreply.github.com>
2026-02-10 15:20:24 +0530

    Create 21-MergeTwoSortedLists

---
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.java
@@ -0,0 +1,62 @@
+// Last updated: 2/10/2026, 3:20:27 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        // // With using Extra Space 
+        // ListNode temp1 = list1;
+        // ListNode temp2 = list2;
+        // ListNode head = new ListNode(100);
+        // ListNode temp = head;
+        // while(temp1 != null && temp2 != null){
+        //     if(temp1.val < temp2.val){
+        //         ListNode a = new ListNode(temp1.val);
+        //         temp.next = a;
+        //         temp = a;
+        //         temp1 = temp1.next;
+        //     } else {
+        //         ListNode a = new ListNode(temp2.val);
+        //         temp.next = a;
+        //         temp = a;
+        //         temp2 = temp2.next;
+        //     }
+        // }
+        // if(temp1 == null){
+        //     temp.next = temp2;
+        // } else {
+        //     temp.next = temp1;
+        // }
+        // return head.next;
+
+        //Without using Extra Space
+        ListNode temp1 = list1;
+        ListNode temp2 = list2;
+        ListNode head = new ListNode(100);
+        ListNode temp = head;
+        while(temp1 != null && temp2 != null){
+            if(temp1.val < temp2.val){
+                temp.next = temp1;
+                temp = temp1;
+                temp1 = temp1.next;
+            } else {
+                temp.next = temp2;
+                temp = temp2;
+                temp2 = temp2.next;
+            }
+        }
+        if(temp1 == null){
+            temp.next = temp2;
+        } else {
+            temp.next = temp1;
+        }
+        return head.next;
+    }
+}
