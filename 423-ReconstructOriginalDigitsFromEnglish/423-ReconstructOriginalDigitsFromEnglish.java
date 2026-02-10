d05ea3e70d188a0a6b291081afa1ec101fc48eaf
HardikRaheja21 <151737746+HardikRaheja21@users.noreply.github.com>
2026-02-10 15:17:11 +0530

    Create 423-ReconstructOriginalDigitsFromEnglish

---
+++ 423-ReconstructOriginalDigitsFromEnglish/423-ReconstructOriginalDigitsFromEnglish.java
@@ -0,0 +1,27 @@
+// Last updated: 2/10/2026, 3:17:14 PM
+class Solution {
+    public String originalDigits(String s) {
+        int[] freq = new int[26];
+        for(char ch : s.toCharArray()){
+            freq[ch - 'a']++;
+        }   
+        int[] digits = new int[10];
+        digits[0] = freq['z' - 'a'];
+        digits[2] = freq['w' - 'a'];
+        digits[4] = freq['u' - 'a'];
+        digits[6] = freq['x' - 'a'];
+        digits[8] = freq['g' - 'a'];
+        digits[3] = freq['h' - 'a'] - digits[8];
+        digits[5] = freq['f' - 'a'] - digits[4];
+        digits[7] = freq['s' - 'a'] - digits[6];
+        digits[1] = freq['o' - 'a'] - digits[0] - digits[2] - digits[4];
+        digits[9] = freq['i' - 'a'] - digits[5] - digits[6] - digits[8];
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < 10; i++){
+            while(digits[i]-- > 0){
+                sb.append(i);
+            }
+        }
+        return sb.toString();
+    }
+}
