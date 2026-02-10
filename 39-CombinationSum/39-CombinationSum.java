3bc1af97f4c5874cfbd1a289a87d9b5070456fb1
HardikRaheja21 <151737746+HardikRaheja21@users.noreply.github.com>
2026-02-10 15:20:12 +0530

    Create 39-CombinationSum

---
+++ 39-CombinationSum/39-CombinationSum.java
@@ -0,0 +1,22 @@
+// Last updated: 2/10/2026, 3:20:15 PM
+class Solution {
+    private void findCombinations(int idx, int[] candidates, int target, List<List<Integer>> ans, List<Integer> ds){
+        if(idx == candidates.length){
+            if(target == 0){
+                ans.add(new ArrayList<>(ds));
+            }
+            return;
+        }
+        if(candidates[idx] <= target){
+            ds.add(candidates[idx]);
+            findCombinations(idx, candidates, target - candidates[idx], ans, ds);
+            ds.remove(ds.size() - 1);
+        }
+        findCombinations(idx + 1, candidates, target, ans, ds);
+    }
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        findCombinations(0, candidates, target, ans, new ArrayList<>());
+        return ans;
+    }
+}
