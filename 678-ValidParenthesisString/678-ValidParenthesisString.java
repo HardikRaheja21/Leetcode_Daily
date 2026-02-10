a20467190ef1f4af589512be04a82b621d9295ba
HardikRaheja21 <151737746+HardikRaheja21@users.noreply.github.com>
2026-02-10 15:16:32 +0530

    Create 678-ValidParenthesisString

---
+++ 678-ValidParenthesisString/678-ValidParenthesisString.java
@@ -0,0 +1,22 @@
+// Last updated: 2/10/2026, 3:16:35 PM
+class Solution {
+    public boolean checkValidString(String s) {
+        int minOpen = 0;
+        int maxOpen = 0;
+        for(char ch : s.toCharArray()){
+            if(ch == '('){
+                minOpen++;
+                maxOpen++;
+            } else if(ch == ')'){
+                minOpen--;
+                maxOpen--;
+            } else {
+                minOpen--;
+                maxOpen++;
+            }
+            if(maxOpen < 0) return false;
+            minOpen = Math.max(minOpen, 0);
+        }
+        return minOpen == 0;
+    }
+}
