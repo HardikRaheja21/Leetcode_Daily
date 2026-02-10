2f569f86ecd2b57d95583e23456e6c731e0cd5c8
HardikRaheja21 <151737746+HardikRaheja21@users.noreply.github.com>
2026-02-10 15:13:56 +0530

    Create 1951-FindTheWinnerOfTheCircularGame

---
+++ 1951-FindTheWinnerOfTheCircularGame/1951-FindTheWinnerOfTheCircularGame.java
@@ -0,0 +1,10 @@
+// Last updated: 2/10/2026, 3:13:59 PM
+class Solution {
+    public int findTheWinner(int n, int k) {
+        int winner = 0;
+        for(int i = 2; i <= n; i++){
+            winner = (winner + k) % i;
+        }
+        return winner + 1;
+    }
+}
