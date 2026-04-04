3f215ad5e527db22a069326e47426dfb3cd87446
HardikRaheja21 <151737746+HardikRaheja21@users.noreply.github.com>
2026-04-04 12:46:49 +0530

    Create 4258-ConstructUniformParityArrayIi

---
+++ 4258-ConstructUniformParityArrayIi/4258-ConstructUniformParityArrayIi.java
@@ -0,0 +1,14 @@
+// Last updated: 4/4/2026, 12:46:51 PM
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int min = Integer.MAX_VALUE;
+        for(int num : nums1){
+            min = Math.min(min, num);
+        }
+        if(min % 2 != 0) return true;
+        for(int num : nums1){
+            if(num % 2 != 0) return false;
+        }
+        return true;
+    }
+}
